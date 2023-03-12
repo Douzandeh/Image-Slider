5aaf271cde5615f1c04bedf2fa5ac656db1210f7
Hossein Douzandeh <Douzandeh.k@gmail.com>
2023-03-12 20:18:34 +0330

    script File in function next and prev

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-ECHO is on.
+let i = 0 // current slide
+let j = 4 // total slides
+
+function next () {
+	document.getElementById("content" + (i+1)).classList.remove("active")
+	i = (j + i + 1) % j;
+	document.getElementById("content" + (i+1)).classList.add("active")
+}
+
+function prev() {
+	document.getElementById("content" + (i+1)).classList.remove("active");
+	i = (j + i - 1) % j;
+	document.getElementById("content" + (i+1)).classList.add("active");
+}
